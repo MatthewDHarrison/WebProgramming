c7667c8c8f187e978edabb59c82dd4ae2c96048f
Ming Chow <mchow@cs.tufts.edu>
2018-02-18 01:09:05 -0500

    Removed Google Places API example code

--- examples/google_maps/create_map.js
+++ examples/google_maps/create_map.js
@@ -32,15 +32,6 @@ function getMyLocation() {
 function renderMap() {
 	me = new google.maps.LatLng(myLat, myLng);
 
-	// Using Google Places API to look for restaurants within 1 mi of where I am
-	// Documentation: https://developers.google.com/maps/documentation/javascript/examples/place-search
-	service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: me,
-          radius: 1609, // meters
-          type: ['restaurant']
-        }, callback);
-	
 	// Update map and go there...
 	map.panTo(me);
 	
@@ -56,27 +47,4 @@ function renderMap() {
 		infowindow.setContent(marker.title);
 		infowindow.open(map, marker);
 	});
-
-	function callback(results, status) {
-		if (status === google.maps.places.PlacesServiceStatus.OK) {
-			for (var i = 0; i < results.length; i++) {
-				createMarker(results[i]);
-			}
-		}
-	}
-
-	function createMarker(place) {
-		console.log(place);
-		var placeLoc = place.geometry.location;
-		var marker = new google.maps.Marker({
-			map: map,
-			icon: "food_icon.png",
-			position: place.geometry.location
-		});
-
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.setContent(place.name);
-			infowindow.open(map, this);
-		});
-	}
 }
